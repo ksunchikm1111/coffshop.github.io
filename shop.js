0397590ec3eb446a2b53aa226635bf89cf22f7c7
ksunchikm1111 <166916284+ksunchikm1111@users.noreply.github.com>
2024-04-30 12:47:53 +0300

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,41 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#CD853F";
+
+let item = "";
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
+
+btn1.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали товар латте!");
+        item = "латте";
+        tg.MainButton.show();
+    }
+}
+
+btn2.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали товар капучино!");
+        item = "капучино";
+        tg.MainButton.show();
+    }
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    tg.sendData(item);
+})
